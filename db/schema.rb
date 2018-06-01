a95c6ea887792bab088175b837e7ff2e11e5d73e
diego <diego.mansilla.sistemas@gmail.com>
2018-05-31 20:49:22 -0500

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180501224310) do
+ActiveRecord::Schema.define(version: 20180601010839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,6 +49,24 @@ ActiveRecord::Schema.define(version: 20180501224310) do
   create_table "padres", force: :cascade do |t|
     t.string "name"
     t.string "apellido"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "police", force: :cascade do |t|
+    t.string "nombre"
+    t.string "apellido_paterno"
+    t.string "apellido_materno"
+    t.string "cargo"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "policia", force: :cascade do |t|
+    t.string "nombre"
+    t.string "apellido_paterno"
+    t.string "apellido_materno"
+    t.string "cargo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

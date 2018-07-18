48decfc4b3b93c35f763262085f1806806af4aff
diego <diego.mansilla.sistemas@gmail.com>
2018-07-18 10:19:34 -0500

    add metodos to model

--- app/models/curso.rb
+++ app/models/curso.rb
@@ -1,2 +1,5 @@
 class Curso < ApplicationRecord
+  def curso_with_grado
+    "#{numero}. #{grado}"
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,6 @@
 class Student < ApplicationRecord
  has_many :inscripcions
+ def nombre_completely
+  "#{nombre}. #{apellido_paterno}. #{apellido_materno}"
+ end
 end

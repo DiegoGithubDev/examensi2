4c405d174d087e40ea8da8a23c29d6a2d04d007b
diego <diego.mansilla.sistemas@gmail.com>
2018-07-18 07:44:32 -0500

    add field grado to course

---
+++ db/migrate/20180718124223_add_grado_to_courses.rb
@@ -0,0 +1,5 @@
+class AddGradoToCourses < ActiveRecord::Migration[5.1]
+  def change
+    add_column :courses, :grado, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180717221453) do
+ActiveRecord::Schema.define(version: 20180718124223) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20180717221453) do
     t.bigint "teacher_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "grado"
     t.index ["teacher_id"], name: "index_courses_on_teacher_id"
   end
 

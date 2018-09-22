8d8ff6fbd693d2be500af5cc46a130d32ae9de6e
diegoMansillaChavez <diego.mansilla.sistemas@gmail.com>
2018-09-22 18:13:09 -0500

    create controller

---
+++ app/helpers/rastreos_helper.rb
@@ -0,0 +1,2 @@
+module RastreosHelper
+end
---
+++ test/controllers/rastreos_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RastreosControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end

be32b2b9f8c2382f3cca0edfef0462134b05e4bf
diegoMansillaChavez <diego.mansilla.sistemas@gmail.com>
2018-09-23 10:17:26 -0500

    save coordenadsaas

--- app/controllers/rastreos_controller.rb
+++ app/controllers/rastreos_controller.rb
@@ -1,2 +1,13 @@
 class RastreosController < ApplicationController
+  def create
+    @empleado = Empleado.find(params[:empleado_id])
+    @rastreo = Rastreo.new(rastreo_params)
+    @rastreo.save
+    redirect_to empleado_path(@empleado)
+  end
+
+  private
+  def rastreo_params
+    params.require(:rastreo).permit(:latitud, :longitud, :image)
+  end
 end
